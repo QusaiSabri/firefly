78c915eb74d099da2de77725797d502dff99e7c1
Qusai Sabri <qusai.sabri19@gmail.com>
2025-05-04 13:52:54 -0500

    Add JSON serialization for image upload response

--- firefly/Models/Responses/UploadImageToFireFlyResponse.cs
+++ firefly/Models/Responses/UploadImageToFireFlyResponse.cs
@@ -1,12 +1,16 @@
-﻿namespace firefly.Models.Responses
+﻿using System.Text.Json.Serialization;
+
+namespace firefly.Models.Responses
 {
     public class UploadImageToFireFlyResponse
     {
+        [JsonPropertyName("images")]
         public List<UploadedImage> Images { get; set; } = new();
     }
 
     public class UploadedImage
     {
+        [JsonPropertyName("id")]
         public string Id { get; set; } = default!;
     }
 }
